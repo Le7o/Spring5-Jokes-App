d3fde10d5c2ff159bac564b0ffff278c8878f434
Thibaud Demijolla <tdemijolla@NX-DOMAIN.LAN>
2020-06-30 18:13:20 +0200

    first commit

---
+++ src/test/java/fr/le7o/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.le7o.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
